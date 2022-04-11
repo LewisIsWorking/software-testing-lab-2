6992f294de54ca31193e0d123fc3e0f607206ae7
Lewis <lcreelman01@qub.ac.uk>
2022-04-11 19:26:36 +0100

    Created RangeTest.java and renamed the test directory to lab2tests.

---
+++ src/test/java/org/jfree/data/lab2tests/RangeTest.java
@@ -0,0 +1,96 @@
+package org.jfree.data.lab2tests;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RangeTest extends TestCase {
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+    void getLowerBound() {
+
+    }
+
+    @Test
+    void getUpperBound() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void getCentralValue() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void intersects() {
+    }
+
+    @Test
+    void testIntersects() {
+    }
+
+    @Test
+    void constrain() {
+    }
+
+    @Test
+    void combine() {
+    }
+
+    @Test
+    void combineIgnoringNaN() {
+    }
+
+    @Test
+    void expandToInclude() {
+    }
+
+    @Test
+    void expand() {
+    }
+
+    @Test
+    void shift() {
+    }
+
+    @Test
+    void testShift() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void isNaNRange() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
